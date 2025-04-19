fc2c45a8f8be5b8178f3c32d665b849a87cdbadf
llj <babylinkbabylink@gmail.com>
2025-04-19 23:55:26 +0800

    feat: ingrone eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Ignore ESLint errors when building
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
