44214a4044b8137f2f626c32c993083e0031b8a7
llj <babylinkbabylink@gmail.com>
2025-04-20 00:53:00 +0800

    fix next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
+    // Ignore ESLint errors when building
     ignoreDuringBuilds: true,
   },
   typescript: {
