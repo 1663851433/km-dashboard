0fa9cd922ca4c9e600d093759c3d3c81828681d5
ayayag <gang.lv@pitayagames.com>
2025-08-20 15:13:09 +0800

    feat: 修复库存表组件数据显示问题

--- app/weeklyPurchaseOld/components/InventoryTable.tsx
+++ app/weeklyPurchaseOld/components/InventoryTable.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 
+import { HddTwoTone, getTwoToneColor, setTwoToneColor } from "@ant-design/icons";
 import { Card, Table, Typography } from "antd";
-import type { ColumnsType } from "antd/es/table";
+import type { TableProps } from "antd";
 
 import { useWeeklyDashboardStores } from "@/src/stores/useWeeklyDashboardStores";
 
 interface DataType {
   key: string;
   product: string;
-  jinan: number;
-  xinmao: number;
-  hunan: number;
-  guangxi: number;
-  puyuan: number;
-  sealed: number;
+  金安: number;
+  新茂: number;
+  湖南: number;
+  广西: number;
+  普源: number;
+  封开: number;
 }
 
-const columns: ColumnsType<DataType> = [
+const columns: TableProps<DataType>["columns"] = [
   {
     title: "",
     dataIndex: "product",
@@ -34,32 +35,41 @@ const columns: ColumnsType<DataType> = [
   },
   {
     title: "湖南",
-    dataIndex: "新茂",
-    key: "新茂",
+    dataIndex: "湖南",
+    key: "湖南",
   },
   {
     title: "广西",
-    dataIndex: "新茂",
-    key: "新茂",
+    dataIndex: "广西",
+    key: "广西",
   },
   {
-    title: "普源",
-    dataIndex: "新茂",
-    key: "新茂",
+    title: "普洱",
+    dataIndex: "普洱",
+    key: "普洱",
   },
   {
     title: "封开",
-    dataIndex: "sealed",
-    key: "sealed",
+    dataIndex: "封开",
+    key: "封开",
   },
 ];
 
+/**
+ * @description 分公司库存
+ */
 const InventoryTable: React.FC = () => {
   const branchInventory = useWeeklyDashboardStores((state) => state.branchInventory);
 
   return (
     <Card className="shadow-md h-[425px]">
-      <Typography.Title level={2}>分公司库存</Typography.Title>
+      <div className="flex items-center mb-[10px]">
+        <HddTwoTone className="text-[32px]" />
+        <Typography.Title level={3} style={{ marginBottom: 0 }}>
+          分公司库存
+        </Typography.Title>
+      </div>
+
       <Table
         columns={columns}
         dataSource={branchInventory}
